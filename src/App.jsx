39bc91ca6d073587cee87b1fc1434feb5a5f9d29
Bismay.exe <bismaysundarmahanta@gmail.com>
2026-07-25 16:34:58 +0530

    feat(routing): set up React Router with Dashboard and Settings routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import Dashboard from './pages/Dashboard'
 import Settings from './pages/Settings'
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Dashboard />,
+  },
+  {
+    path: '/settings',
+    element: <Settings />,
+  },
+])
+
 const App = () => {
-  return <Settings />
+  return <RouterProvider router={router} />
 }
 
 export default App
